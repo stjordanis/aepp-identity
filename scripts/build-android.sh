8e20795c6455f9e35f41e43f1a86483a390e7533
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2018-07-24 16:21:13 +0200

    Fix `cordova prepare` by credential caching
    For some reason `git` process called by `cordova prepare android` ignores `GIT_ASKPASS` environment variable

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 export GIT_ASKPASS=scripts/git-askpass.sh
+git config credential.helper 'cache --timeout=3600'
+git ls-remote -ht https://github.com/aeternity/cordova-plugin-airgap-secure-storage.git
 
 npm config set unsafe-perm true # https://github.com/npm/npm/issues/17346
 npm install
