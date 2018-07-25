decfce28fc8155057a758a96eb3f2ff8f01397cc
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2018-07-25 07:49:23 +0200

    Run "cordova plugin add" with private plugin URL with username

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -9,6 +9,7 @@ npm install
 npx cordova telemetry off
 
 npm run build:cordova
+npx cordova plugin add git+https://$GIT_USERNAME:$GIT_PASSWORD@github.com/aeternity/cordova-plugin-airgap-secure-storage.git
 npx cordova prepare android
 npm run gen:cordova-resources
 npx cordova build android -- --gradleArg=--no-daemon
